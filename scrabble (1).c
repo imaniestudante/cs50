0bcded84527d7f191f0d37970d84596f976deab7
imaniestudante <126624997+imaniestudante@users.noreply.github.com>
2023-08-25 14:42:53 -0300

    Exercício arrays

---
+++ scrabble (1).c
@@ -0,0 +1,50 @@
+//scrabble
+
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+
+string word1 = get_string("Player 1: ");
+string word2 = get_string("Player 2: ");
+
+int score1 = compute_score(word1);
+int score2 = compute_score(word2);
+
+if (score1 > score2)
+{
+    printf("Player 1 wins!\n");
+}
+else if (score1 < score2)
+{
+    printf("Player 2 wins!\n");
+}
+else if (score1 == score2)
+{
+    printf("Tie!\n");
+}
+}
+
+int compute_score(string word)
+{
+string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int pontos = 0;
+for (int i = 0, n = strlen(word); i < n; i++)
+{
+for (int j = 0; j < 26; j++)
+{
+if (toupper(word[i]) == abc[j])
+{
+pontos += POINTS[j];
+}
+}
+}
+return pontos;
+}
